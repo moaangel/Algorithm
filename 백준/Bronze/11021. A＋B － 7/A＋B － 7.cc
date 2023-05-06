6492700f780e234c39bcfcfe2311d407d896213e
moaangel <126864067+moaangel@users.noreply.github.com>
2023-05-07 01:14:09 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int T, A, B;
+	cin >> T;
+	for (int i = 1; i <= T; i++)
+	{
+		cin >> A >> B;
+		cout << "Case #" << i << ": " << A + B << '\n';
+	}
+}
