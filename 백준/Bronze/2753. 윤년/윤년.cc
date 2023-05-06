72f01fc76f124b09c9b67eb01f14ce8a1ecc2c9d
moaangel <126864067+moaangel@users.noreply.github.com>
2023-05-07 01:08:52 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	int A;
+	cin >> A;
+	if ((A % 4) == 0 && A % 100 != 0 || A % 400 == 0)
+		cout << "1" << endl;
+	else
+		cout << "0" << endl;
+}
