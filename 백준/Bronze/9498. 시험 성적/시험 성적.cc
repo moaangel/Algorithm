aa607b5302963631871276b37ef3124247cca742
moaangel <126864067+moaangel@users.noreply.github.com>
2023-05-07 01:08:44 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	int A;
+	cin >> A;
+	if (A >= 90 && A <= 100)
+		cout << "A" << endl;
+	else if (A >= 80 && A <= 89)
+		cout << "B" << endl;
+	else if (A >= 70 && A <= 79)
+		cout << "C" << endl;
+	else if (A >= 60 && A <= 69)
+		cout << "D" << endl;
+	else
+		cout << "F" << endl;
+}
