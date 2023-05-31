f7958794e04732811dcc5065990f3fe87f66aeda
moaangel <126864067+moaangel@users.noreply.github.com>
2023-05-31 10:55:46 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int a[104][104];
+int h, w;
+string s;
+int main() {
+	cin >> h >> w;
+	for (int i = 0; i < h; ++i) {
+		cin >> s;
+		for (int j = 0; j < w; ++j) {
+			if (s[j] == '.') a[i][j] = -1;
+			else a[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < h; ++i) {
+		for (int j = 0; j < w; ++j) {
+			if (a[i][j] == 0) {
+				int cnt = 1;
+				while (a[i][j + 1] == -1) {
+					a[i][j+1] = cnt++;
+					j++;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < h; ++i) {
+		for (int j = 0; j < w; ++j) {
+			cout << a[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
