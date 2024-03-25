5ea55a5cf5144055bf7a4883ead8e67ea69e8960
moaangel <126864067+moaangel@users.noreply.github.com>
2024-03-25 21:27:01 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 16 ms, Memory: 5148 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m,ret;
+int visited[200004];
+int cnt[200004];
+int pre[200004];
+int main() {
+	cin >> n >> m;
+
+
+	visited[n] = 1;
+	cnt[n] = 1;
+	queue<int> q;
+	vector<int> v;
+	q.push(n);
+	while (q.size()) {
+		int num = q.front();
+		q.pop();
+		if (num == m) {
+			ret = visited[m];
+			break;
+		}
+		for (int i = 0; i < 3; i++) {
+			int newnum;
+			if (i == 0)
+				newnum = num + 1;
+			if (i == 1)
+				newnum = num - 1;
+			if (i == 2)
+				newnum = num * 2;
+
+			if (newnum < 0 || newnum > 200004)continue;
+			if (!visited[newnum]) {
+				q.push(newnum);
+				visited[newnum] = visited[num] + 1;
+				pre[newnum] = num;
+			}
+		}
+	}
+	for (int i = m; i != n; i = pre[i]) {
+		v.push_back(i);
+	}
+	v.push_back(n);
+	cout << ret - 1 << '\n';
+	reverse(v.begin(), v.end());
+	for (int i : v) cout << i << ' ';
+	return 0;
+}
