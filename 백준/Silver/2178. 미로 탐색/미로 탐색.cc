1e8f9acba977adc0f5f1010ace11362610941534
moaangel <126864067+moaangel@users.noreply.github.com>
2024-05-13 22:55:01 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,42 +1,40 @@
 #include <iostream>
-#include <stdio.h>
+#include <string>
 #include <queue>
-#include <tuple>
-
 using namespace std;
 
+int dy[4] = { 0,1,0,-1 };
+int dx[4] = { 1,0,-1,0 };
+
+int a[102][102];
+int visited[102][102];
 int n, m;
-int a[105][105];
-int visited[105][105];
-
-int dx[] = { 0,1,0,-1 };
-int dy[] = { -1,0,1,0 };
-
 int main() {
-	scanf("%d %d", &n, &m);
+	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
 		for (int j = 0; j < m; j++) {
-			scanf("%1d", &a[i][j]);
+			a[i][j] = s[j] - '0';
 		}
-		
 	}
-	int x=0, y=0;
-	queue<pair<int, int>> q;
-	visited[x][y] = 1;
-	q.push({ x,y });
+	visited[0][0] = 1;
+	queue<pair<int, int>>q;
+	q.push({ 0,0 });
 	while (q.size()) {
-		tie(x, y) = q.front();
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny]) continue;
-			if (a[nx][ny] == 1) {
-				visited[nx][ny] += visited[x][y] + 1;
-				q.push({ nx, ny });
-			}
-		}
+		int y = q.front().first;
+		int x = q.front().second;
 
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m)continue;
+			if (a[ny][nx] == 0 || visited[ny][nx]) continue;
+			visited[ny][nx] = visited[y][x] + 1;
+			q.push({ ny,nx });
+		}
 	}
-	printf("%d", visited[n - 1][m - 1]);
+	cout << visited[n - 1][m - 1] << '\n';
 }
