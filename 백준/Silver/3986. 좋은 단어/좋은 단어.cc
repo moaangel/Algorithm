ac889daf361bd39f552df705885987c3767af645
moaangel <126864067+moaangel@users.noreply.github.com>
2024-05-13 22:33:07 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,21 +1,24 @@
 #include <iostream>
-#include <string>
 #include <stack>
-int n,cnt;
-int main() {
-	std::cin >> n;
+#include <string>
+using namespace std;
 
+
+int n,ret;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
 	for (int i = 0; i < n; i++) {
-		std::string str;
-		std::cin >> str;
-		std::stack<char> stk;
-		for (char a : str) {
-			if (stk.size() && stk.top() == a) // size 사용 이유:참조오류 방지(스택에 값이 있는가)
-				stk.pop();
-			else
-				stk.push(a);
+		string s;
+		stack<char> stk;
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			if (stk.size() && stk.top() == s[j])stk.pop();
+			else stk.push(s[j]);
 		}
-		if (stk.size() == 0)cnt++;
+		if (!stk.size())ret++;
 	}
-	std::cout << cnt;
+	cout << ret << '\n';
 }
