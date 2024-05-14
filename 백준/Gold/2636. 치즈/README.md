2ca226b7f0bd8bab7a61c909ed6c367263c938f3
moaangel <126864067+moaangel@users.noreply.github.com>
2024-05-14 14:06:49 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/README.md
+++ 백준/Gold/2636. 치즈/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 2156 KB, 시간: 0 ms
+메모리: 2104 KB, 시간: 0 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2024년 3월 17일 22:28:12
+2024년 5월 14일 14:06:42
 
 ### 문제 설명
 
@@ -30,7 +30,7 @@
 
 <p style="text-align: center;"><그림 2> 한 시간 후의 치즈 모양</p>
 
-<p style="text-align: center;"><img alt="" src="" style="width: 234px; height: 232px;"></p>
+<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/58fc0743-794b-4e27-84e8-fe491ec7bf3d/-/preview/" style="width: 234px; height: 232px;"></p>
 
 <p style="text-align: center;"><그림 3> 두 시간 후의 치즈 모양</p>
 
--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -1,35 +1,15 @@
 #include <iostream>
-#include <vector>
-
+#include <string>
+#include <queue>
 using namespace std;
 
+int dy[4] = { 1,0,-1,0 };
+int dx[4] = { 0,1,0,-1 };
 
-int dy[4] = { 0,1,0,-1 };
-int dx[4] = { 1,0,-1,0 };
-int a[101][101];
-int visited[101][101];
-
-vector<pair<int, int>> v;
-
-int n, m, t, lastsize;
-
-void dfs(int y, int x) {
-	visited[y][x] = 1;
-
-	if (a[y][x] == 1) {
-		v.push_back({ y,x });
-		return;
-	}
-
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-
-		if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] == 1) continue;
-		dfs(ny, nx);
-	}
-}
-
+int n,m,ret,cnt;
+int a[102][102];
+int visited[102][102];
+queue<pair<int, int>> clist;
 int main() {
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
@@ -37,24 +17,35 @@ int main() {
 			cin >> a[i][j];
 		}
 	}
-
+	clist.push({ 0,0 });
 	while (1) {
-		bool flag = 0;
-		v.clear();
-		fill(&visited[0][0], &visited[0][0] + 101 * 101, 0);
-		dfs(0, 0);
-		for (pair<int, int> b : v) {
-			a[b.first][b.second] = 0;
-		}
-		lastsize = v.size();
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (a[i][j] != 0) flag = 1;
+		ret++;
+		vector<pair<int, int>> lostlist;
+		fill(&visited[0][0], &visited[0][0] + 102 * 102, 0);
+		while (clist.size()) {
+			int y = clist.front().first;
+			int x = clist.front().second;
+			clist.pop();
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+
+				if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx])continue;
+				visited[ny][nx] = 1;
+				if (a[ny][nx] == 0) {
+					clist.push({ ny,nx });
+				}
+				else lostlist.push_back({ ny,nx });
 			}
 		}
-		t++;
-		if (!flag)break;
-		
+		if (lostlist.size()) {
+			for (pair<int, int> num : lostlist) {
+				a[num.first][num.second] = 0;
+			}
+			cnt = lostlist.size();
+			clist.push({ 0,0 });
+		}
+		else break;
 	}
-	cout << t << "\n" << lastsize << endl;
+	cout << ret-1 << '\n' << cnt << '\n';
 }
