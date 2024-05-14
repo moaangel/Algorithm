b186d49d56248b37c59feb877404ae201f631993
moaangel <126864067+moaangel@users.noreply.github.com>
2024-05-14 15:58:45 +0900

    [Gold V] Title: 보물섬, Time: 132 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,63 +1,57 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <tuple>
-#include <string>
 #include <algorithm>
+#include <queue>
+#include <string>
+#include <vector>
 using namespace std;
-
-int n, m, ret;
-int a[51][51];
-int visited[51][51];
 
 int dy[4] = { 0,1,0,-1 };
 int dx[4] = { 1,0,-1,0 };
 
-void bfs(int y, int x) {
-	queue<pair<int,int>> q;
-	visited[y][x] = 1;
+queue<pair<int, int>> q;
+vector<pair<int, int>> v;
+char a[52][52];
+int visited[52][52];
+int n, m, ret;
 
-	q.push({ y,x });
+void bfs(int sy, int sx) {
+	visited[sy][sx] = 1;
+	q.push({ sy, sx });
+
 	while (q.size()) {
-		tie(y, x) = q.front();
+		int y = q.front().first;
+		int x = q.front().second;
 		q.pop();
 
 		for (int i = 0; i < 4; i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
 
-			if (nx < 0 || ny < 0 || ny >= n || nx >= m || visited[ny][nx])continue;
-			if (a[ny][nx] == 0)continue;
-
-			visited[ny][nx] = visited[y][x] + 1;
-			ret = max(ret, visited[ny][nx]);
-			q.push({ ny, nx });
-			
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m)continue;
+			if (a[ny][nx] == 'L' && !visited[ny][nx]) {
+				visited[ny][nx] = visited[y][x] + 1;
+				ret = max(ret, visited[ny][nx] - 1);
+				q.push({ ny,nx });
+			}
 		}
 	}
 }
 
 int main() {
 	cin >> n >> m;
-
 	for (int i = 0; i < n; i++) {
 		string s;
 		cin >> s;
 		for (int j = 0; j < m; j++) {
-			if (s[j] == 'W')
-				a[i][j] = 0;
-			if (s[j] == 'L')
-				a[i][j] = 1;
+			a[i][j] = s[j];
+			if (s[j] == 'L')v.push_back({ i,j });
 		}
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (a[i][j] == 1) {
-				bfs(i, j);
-				fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
-			}
-		}
+	for (pair<int, int> i : v) {
+		fill(&visited[0][0], &visited[0][0] + 52 * 52, 0);
+		bfs(i.first, i.second);
 	}
-	cout << ret-1 << endl;
+
+	cout << ret << '\n';
 }
