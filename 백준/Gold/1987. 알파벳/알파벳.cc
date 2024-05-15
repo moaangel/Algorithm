99a5f467cb1662b0a1ea53abba4c637b4afab3d6
moaangel <126864067+moaangel@users.noreply.github.com>
2024-05-15 13:45:43 +0900

    [Gold IV] Title: 알파벳, Time: 436 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,37 +1,29 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <string>
-
+#include <algorithm>
 using namespace std;
-
 
 int dy[4] = { 0,1,0,-1 };
 int dx[4] = { 1,0,-1,0 };
 
-char a[21][21];
-int visited[21][21];
-int alphabet[200];
+int visited[26];
+char a[22][22];
+int n, m,ret;
 
-int n, m, cnt;
+void dfs(int y, int x, int cnt) {
+	ret = max(ret, cnt);
 
-int dfs(int y, int x) {
-	if (visited[y][x] > cnt)
-		cnt = visited[y][x];
 	for (int i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
 		if (ny < 0 || nx < 0 || ny >= n || nx >= m)continue;
-		if (alphabet[a[ny][nx]] == 1)continue;
-		visited[ny][nx] = visited[y][x] + 1;
-		alphabet[a[ny][nx]] = 1;
-		
-		dfs(ny, nx);
-		alphabet[a[ny][nx]] = 0;
+		if (visited[a[ny][nx] - 'A']) continue;
+		visited[a[ny][nx] - 'A'] = 1;
+		dfs(ny, nx, cnt + 1);
+		visited[a[ny][nx] - 'A'] = 0;
 	}
-	return cnt;
 }
+
 int main() {
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
@@ -39,9 +31,7 @@ int main() {
 			cin >> a[i][j];
 		}
 	}
-
-	visited[0][0] = 1;
-	alphabet[a[0][0]] = 1;
-
-	cout << dfs(0, 0) << endl;
+	visited[a[0][0] - 'A'] = 1;
+	dfs(0, 0, 1);
+	cout << ret << '\n';
 }
