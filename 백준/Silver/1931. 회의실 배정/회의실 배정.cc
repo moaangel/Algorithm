1cacee396a0e7c10007d807a978cb2854d4fe3bd
moaangel <126864067+moaangel@users.noreply.github.com>
2024-05-15 20:45:07 +0900

    [Silver I] Title: 회의실 배정, Time: 84 ms, Memory: 3572 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,29 +1,28 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-int n,ret = 1;
+int n;
 vector<pair<int, int>> v;
 int main() {
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		int a, b;
-		cin >> a >> b;
-		v.push_back({ b,a });
+		int s, e;
+		cin >> s >> e;
+		v.push_back({ e,s });
 	}
 	sort(v.begin(), v.end());
 	int s = v[0].second;
 	int e = v[0].first;
-	for (int i = 1; i < n; i++) {
+	int ret = 1;
+	for (int i = 1; i < v.size(); i++) {
 		if (v[i].second >= e) {
 			ret++;
 			s = v[i].second;
 			e = v[i].first;
 		}
-		else
-			continue;
+		else continue;
 	}
 	cout << ret << '\n';
 }
