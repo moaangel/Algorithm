5e1eef0c3ed999d69746101722510575a82463ff
moaangel <126864067+moaangel@users.noreply.github.com>
2024-05-18 22:03:54 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 45 ms, Memory: 13,552 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.cpp
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.cpp
@@ -1,41 +1,28 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 #include <algorithm>
-#include <string>
 using namespace std;
 
-int n;
-int a[1002];
 int visited[1002];
-int main(int argc, char** argv)
-{
-	int test_case;
-	int T;
-
-	//freopen("input.txt", "r", stdin);
+int a[1002];
+int T,n;
+int main() {
 	cin >> T;
-
-	for (test_case = 1; test_case <= T; ++test_case)
-	{
-		fill(visited, visited + 1002, 1);
-		int ret = 0;
+	for (int tc = 1; tc <= T; tc++) {
 		cin >> n;
+		fill(visited, visited + 1002, 1);
 		for (int i = 0; i < n; i++) {
-			int num;
-			cin >> num;
-			a[i] = num;
+			cin >> a[i];
 		}
 		for (int i = 1; i < n; i++) {
-			for (int j = i-1; j >= 0; j--) {
-				if (a[i] > a[j] && visited[i] <= visited[j]) {
-					visited[i] = visited[j] + 1;
-				}
+			for (int j = i - 1; j >= 0; j--) {
+				if (a[i] > a[j])
+					visited[i] = max(visited[i], visited[j] + 1);
 			}
 		}
+		int ret = 0;
 		for (int i = 0; i < n; i++) {
 			ret = max(ret, visited[i]);
 		}
-		cout << "#" << test_case << " " << ret << '\n';
+		cout << "#" << tc << " " << ret << '\n';
 	}
-	return 0;
 }
