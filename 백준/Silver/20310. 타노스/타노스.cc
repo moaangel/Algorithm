e6139672c782ab63cb6633a31d005f8ba9ce4972
moaangel <126864067+moaangel@users.noreply.github.com>
2025-05-20 22:32:04 +0900

    [Silver III] Title: 타노스, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.cc
+++ 백준/Silver/20310. 타노스/타노스.cc
@@ -14,10 +14,21 @@ int main() {
 	}
 	arr[0] /= 2;
 	arr[1] /= 2;
-
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < arr[i]; j++) {
-			cout << i;
+	string ans = "";
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == '0') {
+			if (arr[0]) {
+				ans += '0';
+				arr[0]--;
+			}
+		}
+		else {
+			if (arr[1]) {
+				arr[1]--;
+			}
+			else ans += '1';
 		}
 	}
+	cout << ans << endl;
+
 }
