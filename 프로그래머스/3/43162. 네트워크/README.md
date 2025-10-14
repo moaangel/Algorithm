5abf9d7b703384b8de64f69d79b036ef42b08009
moaangel <126864067+moaangel@users.noreply.github.com>
2025-10-15 00:53:27 +0900

    [level 3] Title: 네트워크, Time: 2.37 ms, Memory: 6.52 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=cpp) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 0.13 ms
+메모리: 6.52 MB, 시간: 2.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 21일 23:26:05
+2025년 10월 15일 00:53:25
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -4,27 +4,24 @@
 using namespace std;
 
 int visited[202];
-int ret =0;
 
-void dfs(int idx, vector<vector<int>> computers){
-    for(int i = 0; i < computers[idx].size(); i++){
-        if (computers[idx][i] && !visited[i]){
-            visited[i] = 1;
-            dfs(i, computers);
-        }
+void dfs(int idx, vector<vector<int>> computers) {
+    for (int i=0; i<computers.size(); i++){
+        if(i == idx || computers[idx][i] == 0 || visited[i])continue;
+        visited[i] = 1;
+        dfs(i,computers);
     }
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    for (int i=0; i<computers.size(); i++){
-        if (!visited[i]){
+    for (int i=0; i<n; i++){
+        if(visited[i] == 1)continue;
+        else{
+            answer++;
             visited[i] = 1;
-            ret++;
-            dfs(i, computers);
+            dfs(i,computers);
         }
-        
     }
-    answer = ret;
     return answer;
 }
